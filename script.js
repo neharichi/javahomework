f190e39ba0e8acfec9a6d5b60415618e11a0d009
neharichi <54488933+neharichi@users.noreply.github.com>
2019-10-13 19:28:26 -0700

    java work

---
+++ script.js
@@ -0,0 +1,25 @@
+function generatePassword(len) {
+    var length = len,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("password").value = retVal;
+}
+
+
+function CopyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("password");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
